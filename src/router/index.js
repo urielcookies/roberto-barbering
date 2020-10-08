528050f122c01125afc7b33076cfdba5952a59e9
Ever Uriel Garcia <uriel621@live.com>
2020-10-08 01:00:23 -0600

    Add calendar view

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
+import Calendar from "../views/Calendar.vue";
 
 const routes = [
   {
@@ -21,6 +22,11 @@ const routes = [
     path: "/login",
     name: "Login",
     component: Login
+  },
+  {
+    path: "/calendar",
+    name: "Calendar",
+    component: Calendar
   }
 ];
 
---
+++ src/views/Calendar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Calendar Page</h1>
+  </div>
+</template>
+
+<script>
+console.log("SUP");
+</script>
